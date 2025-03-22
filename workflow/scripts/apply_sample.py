fix scaling

workflow/scripts/apply_sample.py
@@ -60,7 +60,7 @@ class CapitalCostCache:
         assert self.is_valid_data()
         if self.capital_cost:
             logger.info("Returning pre-defined capital cost.")
-            return round(self.capital_cost, 2)
+            return round(self.capital_cost, 5)
         elif transport:
             return self._calc_transport_capex()
         else:
@@ -73,9 +73,9 @@ class CapitalCostCache:
 
         if self.itc:
             assert self.itc < 1  # ensures is per_unit
-            return round(capex * (1 - self.itc), 2)
+            return round(capex * (1 - self.itc), 5)
         else:
-            return round(capex, 2)
+            return round(capex, 5)
 
     def _calc_transport_capex(self) -> float:
         """OCC comes as 'usd' and needs to be converted to usd/kvmt."""
@@ -85,7 +85,7 @@ class CapitalCostCache:
         assert self.vmt_per_year
         capex = ((self.occ / self.vmt_per_year) + self.fixed_cost) * annuity
 
-        return round(capex, 2)
+        return round(capex, 5)
 
 @dataclass
 class MethaneLeakageCache:
@@ -106,7 +106,7 @@ class MethaneLeakageCache:
 
     def calculate_leakage(self) -> float:
         assert self.leakage < 1  # confirm per_unit
-        return round(self.gwp * self.leakage, 2)
+        return round(self.gwp * self.leakage, 5)
 
 def is_valid_carrier(n: pypsa.Network, params: pd.DataFrame) -> bool:
     """Check all defined carriers are in the network."""
@@ -233,9 +233,9 @@ def _apply_static_sample(
         getattr(n, c).loc[slicer, attr] = ref_slice + ref_slice.mul(multiplier)
 
     return {
-        "ref": round(ref, 2),  # original value applied to network
-        "scaled": round(sampled, 2),  # new value applied to network
-        "difference": round(diff, 2),  # percent diff between original and new
+        "ref": round(ref, 5),  # original value applied to network
+        "scaled": round(sampled, 5),  # new value applied to network
+        "difference": round(diff, 5),  # percent diff between original and new
     }
 
 def _apply_dynamic_sample(
@@ -265,9 +265,9 @@ def _apply_dynamic_sample(
     assert scaled is not np.nan, f"Scaled {car} {attr} is np.nan"
 
     return {
-        "ref": round(ref, 2),  # original mean value applied to network
-        "scaled": round(scaled, 2),  # new mean value applied to network
-        "difference": round(diff, 2),  # percent diff between original and new
+        "ref": round(ref, 5),  # original mean value applied to network
+        "scaled": round(scaled, 5),  # new mean value applied to network
+        "difference": round(diff, 5),  # percent diff between original and new
     }
 
 
@@ -439,7 +439,7 @@ def _get_constraint_sample(
     sampled = {
         "ref": ref,
         "scaled": scaled,
-        "difference": round(abs(scaled - ref) / ref * 100, 2),
+        "difference": round(abs(scaled - ref) / ref * 100, 5),
     }
 
     return meta, sampled
@@ -485,7 +485,7 @@ def apply_sample(
         car = data["carrier"]
         attr = data["attribute"]
         absolute = True if data["range"] == "absolute" else False
-        value = round(data["value"][run], 2)
+        value = round(data["value"][run], 5)
 
         # if the applied value is an intermediate calculation
         if attr in CACHED_ATTRS and absolute:
@@ -666,5 +666,5 @@ if __name__ == "__main__":
             meta_df = pd.DataFrame.from_dict(meta).T
             meta_df.to_csv(meta_save_name, index=True)
 
-    ss = pd.DataFrame(scaled_sample, columns=sample.columns).round(3)
+    ss = pd.DataFrame(scaled_sample, columns=sample.columns).round(5)
     ss.to_csv(scaled_sample_file, index=False)

workflow/scripts/solve.py
@@ -289,17 +289,17 @@ def add_technology_capacity_target_constraints(
         )
 
         if target["max"] == "existing":
-            target["max"] = round(lhs_existing, 2) + 0.01
+            target["max"] = round(lhs_existing, 5) + 0.01
         else:
             target["max"] = float(target["max"])
 
         if target["min"] == "existing":
-            target["min"] = round(lhs_existing, 2) - 0.01
+            target["min"] = round(lhs_existing, 5) - 0.01
         else:
             target["min"] = float(target["min"])
 
         if not np.isnan(target["min"]):
-            rhs = target["min"] - round(lhs_existing, 2)
+            rhs = target["min"] - round(lhs_existing, 5)
 
             n.model.add_constraints(
                 lhs >= rhs,
@@ -321,7 +321,7 @@ def add_technology_capacity_target_constraints(
                 target["max"] >= lhs_existing
             ), f"TCT constraint of {target['max']} MW for {target['carrier']} must be at least {lhs_existing}"
 
-            rhs = target["max"] - round(lhs_existing, 2)
+            rhs = target["max"] - round(lhs_existing, 5)
 
             n.model.add_constraints(
                 lhs <= rhs,
@@ -903,8 +903,8 @@ if __name__ == "__main__":
     extra_fn["ng_trade"] = {}
     extra_fn["ng_trade"]["domestic"] = pd.read_csv(ng_dommestic_f, index_col=0)
     extra_fn["ng_trade"]["international"] = pd.read_csv(ng_international_f, index_col=0)
-    imports = constraints[constraints.attribute == "nat_gas_import"].round(2)
-    exports = constraints[constraints.attribute == "nat_gas_export"].round(2)
+    imports = constraints[constraints.attribute == "nat_gas_import"].round(5)
+    exports = constraints[constraints.attribute == "nat_gas_export"].round(5)
 
     if len(imports) == 1:
         extra_fn["ng_trade"]["min_import"] = imports.value.values[0]
@@ -929,7 +929,7 @@ if __name__ == "__main__":
     ###
     extra_fn["gshp"] = {}
     extra_fn["gshp"]["data"] = pd.read_csv(pop_f)
-    gshp_sample = constraints[constraints.attribute == "gshp"].round(2)
+    gshp_sample = constraints[constraints.attribute == "gshp"].round(5)
     # in sanitize_params we already check that gshp is defined correctly
     # We need to move the res and com gshp capacites together tho.
     gshp_sample = gshp_sample.drop_duplicates(subset="attribute")
@@ -946,7 +946,7 @@ if __name__ == "__main__":
     ###
     extra_fn["rps"] = {}
     extra_fn["rps"]["data"] = pd.read_csv(rps_f)
-    rps_sample = constraints[constraints.attribute == "rps"].round(2)
+    rps_sample = constraints[constraints.attribute == "rps"].round(5)
 
     if len(rps_sample) == 1:
         extra_fn["rps"]["sample"] = rps_sample.value.values[0]
@@ -960,7 +960,7 @@ if __name__ == "__main__":
     ###
     extra_fn["ces"] = {}
     extra_fn["ces"]["data"] = pd.read_csv(ces_f)
-    ces_sample = constraints[constraints.attribute == "ces"].round(2)
+    ces_sample = constraints[constraints.attribute == "ces"].round(5)
 
     if len(ces_sample) == 1:
         extra_fn["ces"]["sample"] = ces_sample.value.values[0]
@@ -974,7 +974,7 @@ if __name__ == "__main__":
     ###
     extra_fn["tct"] = {}
     extra_fn["tct"]["data"] = pd.read_csv(tct_f)
-    extra_fn["tct"]["sample"] = constraints[constraints.attribute == "tct"].round(2)
+    extra_fn["tct"]["sample"] = constraints[constraints.attribute == "tct"].round(5)
 
     target_names = extra_fn["tct"]["data"].name.to_list()
     sample_names = extra_fn["tct"]["sample"].name.to_list()
@@ -986,7 +986,7 @@ if __name__ == "__main__":
     ###
     extra_fn["co2L"] = {}
 
-    co2_sample = constraints[constraints.attribute == "co2L"].round(2)
+    co2_sample = constraints[constraints.attribute == "co2L"].round(5)
 
     if len(co2_sample) == 1:
         extra_fn["co2L"]["sample"] = co2_sample.value.values[0]
@@ -1001,7 +1001,7 @@ if __name__ == "__main__":
     ###
     extra_fn["lv"] = {}
 
-    lv_sample = constraints[constraints.attribute == "lv"].round(2)
+    lv_sample = constraints[constraints.attribute == "lv"].round(5)
 
     if len(lv_sample) == 1:
         # '1 +' because expansion given as per unit