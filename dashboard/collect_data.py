e48e6e6a8858750a67ad1770c8bf0a43dcbc1e0c
trevorb1 <trevor_barnes@sfu.ca>
2025-09-17 14:48:20 -0700

    update collection for state level

--- dashboard/collect_data.py
+++ dashboard/collect_data.py
@@ -5,7 +5,7 @@ import pandas as pd
 from pathlib import Path
 import json
 
-from components.utils import ISOS
+from components.utils import ISOS, STATES
 
 import logging
 
@@ -45,6 +45,9 @@ PARAM_ATTRIBUTE_NICE_NAMES = {
     "ces": "Constraints",
     "elec_trade": "Constraints",
     "landuse": "Constraints",
+    "rec": "Renewable Energy Certificate",
+    "imports_co2": "Import Emissions",
+    "e_nom": "Natural Gas",  # only one e_nom
 }
 
 PWR_CARRIERS = [
@@ -62,6 +65,8 @@ PWR_CARRIERS = [
     "onwind",
     "solar",
     "waste",
+    "4hr_battery_storage",
+    "8hr_battery_storage",
 ]
 
 
@@ -70,34 +75,34 @@ def _round_df(df: pd.DataFrame) -> pd.DataFrame:
     return df.mask(df.abs() < 1e-8, 0).round(ROUND_TO)
 
 
-def get_param_names(root: Path, iso: str, mode: str) -> dict[str, str]:
+def get_param_names(root: Path, state: str, mode: str) -> dict[str, str]:
     """Gets names and nice_names for the parameter data."""
 
     assert mode in ("gsa", "ua"), f"Invalid mode: {mode}. Must be 'gsa' or 'ua'."
 
-    data_f = Path(root, "results", iso, mode, "parameters.csv")
+    data_f = Path(root, "results", state, mode, "parameters.csv")
     df = pd.read_csv(data_f, index_col=0)[["group", "nice_name"]]
     logger.debug(f"Collecting nice names from {data_f}.")
     df = df.drop_duplicates()
     return df.set_index("group")["nice_name"].to_dict()
 
 
-def get_result_names(root: Path, iso: str, mode: str) -> dict[str, str]:
+def get_result_names(root: Path, state: str, mode: str) -> dict[str, str]:
     """Gets names and nice_names for the result data."""
 
     assert mode in ("gsa", "ua"), f"Invalid mode: {mode}. Must be 'gsa' or 'ua'."
 
-    data_f = Path(root, "results", iso, mode, "results.csv")
+    data_f = Path(root, "results", state, mode, "results.csv")
     df = pd.read_csv(data_f, index_col=0)
     logger.debug(f"Collecting nice names from {data_f}.")
     return df.nice_name.to_dict()
 
 
-def collect_sa(root: Path, iso: str, results: list[str]) -> pd.DataFrame:
+def collect_sa(root: Path, state: str, results: list[str]) -> pd.DataFrame:
     """Collects the SA data from the results directory."""
 
-    data_dir = Path(root, "results", iso, "gsa", "SA")
-    logger.debug(f"Collecting SA data for {iso} from {data_dir}.")
+    data_dir = Path(root, "results", state, "gsa", "SA")
+    logger.debug(f"Collecting SA data for {state} from {data_dir}.")
 
     inputs = [Path(data_dir, f"{x}.csv") for x in results]
 
@@ -108,18 +113,18 @@ def collect_sa(root: Path, iso: str, results: list[str]) -> pd.DataFrame:
         df.name = name
         dfs.append(df)
     df = _round_df(pd.concat(dfs, axis=1))
-    df["iso"] = iso
+    df["state"] = state
     df = df.reset_index(names=["param"])
-    return df.set_index(["param", "iso"])
+    return df.set_index(["param", "state"])
 
 
-def collect_runs(root: Path, iso: str, mode: str, results: list[str]) -> pd.DataFrame:
+def collect_runs(root: Path, state: str, mode: str, results: list[str]) -> pd.DataFrame:
     """Collects the model run results data from the results directory."""
 
     assert mode in ("gsa", "ua"), f"Invalid mode: {mode}. Must be 'gsa' or 'ua'."
 
-    data_dir = Path(root, "results", iso, mode, "results")
-    logger.debug(f"Collecting model run results data for {iso} from {data_dir}.")
+    data_dir = Path(root, "results", state, mode, "results")
+    logger.debug(f"Collecting model run results data for {state} from {data_dir}.")
 
     inputs = [Path(data_dir, f"{x}.csv") for x in results]
 
@@ -134,36 +139,36 @@ def collect_runs(root: Path, iso: str, mode: str, results: list[str]) -> pd.Data
         df = df.rename(columns={"value": name})
         dfs.append(df)
     df = _round_df(pd.concat(dfs, axis=1))
-    df["iso"] = iso
+    df["state"] = state
     df = df.reset_index(names=["run"])
-    return df.set_index(["run", "iso"])
+    return df.set_index(["run", "state"])
 
 
 def get_empty_sa() -> pd.DataFrame:
     """Returns an empty dataframe for the SA data."""
-    return pd.DataFrame(columns=["param", "iso"]).set_index(["param", "iso"])
+    return pd.DataFrame(columns=["param", "state"]).set_index(["param", "state"])
 
 
 def get_empty_run() -> pd.DataFrame:
     """Returns an empty dataframe for the run data."""
-    return pd.DataFrame(columns=["param", "iso"]).set_index(["param", "iso"])
+    return pd.DataFrame(columns=["param", "state"]).set_index(["param", "state"])
 
 
-def get_base_params_file(root: Path, iso: str) -> pd.DataFrame:
-    """Returns the base parameters file for shared between all ISOs."""
-    df = pd.read_csv(Path(root, "results", iso, "gsa", "parameters.csv"))
-    # remove iso specific data
+def get_base_params_file(root: Path, state: str) -> pd.DataFrame:
+    """Returns the base parameters file for shared between all states."""
+    df = pd.read_csv(Path(root, "results", state, "gsa", "parameters.csv"))
+    # remove state specific data
     df = df[~((df.name.str.startswith("tct_")) | (df.name == ("emission_limit")))]
-    df["iso"] = "all"
+    df["state"] = "all"
     return df
 
 
-def get_iso_params(root: Path, iso: str) -> pd.DataFrame:
-    """Returns the parameters file for the given ISO and mode."""
-    df = pd.read_csv(Path(root, "results", iso, "gsa", "parameters.csv"))
-    # track iso specific data
+def get_state_params(root: Path, state: str) -> pd.DataFrame:
+    """Returns the parameters file for the given state and mode."""
+    df = pd.read_csv(Path(root, "results", state, "gsa", "parameters.csv"))
+    # track state specific data
     df = df[(df.name.str.startswith("tct_")) | (df.name == ("emission_limit"))]
-    df["iso"] = iso
+    df["state"] = state
     return df
 
 
@@ -204,8 +209,17 @@ def assign_parameter_filters(params: pd.DataFrame) -> pd.DataFrame:
             return "Power"
         elif carrier == "landuse":
             return "System"
-        elif carrier in ["gas", "gas production", "gas storage", "gas pipeline", "gas imports", "gas exports"]:
+        elif carrier in [
+            "gas",
+            "gas production",
+            "gas storage",
+            "gas pipeline",
+            "gas imports",
+            "gas exports",
+        ]:
             return "Natural Gas"
+        elif carrier == "imports_rec":
+            return "Power"
         else:
             raise ValueError(f"Invalid carrier: {carrier}")
 
@@ -274,10 +288,10 @@ if __name__ == "__main__":
     metadata = json.load(
         open(Path(root, "dashboard", "data", "locked", "metadata.json"))
     )
-    for iso in ISOS:
+    for state in STATES:
         try:
-            params = pd.read_csv(Path(root, "results", iso, "gsa", "parameters.csv"))
-            results = pd.read_csv(Path(root, "results", iso, "ua", "results.csv"))
+            params = pd.read_csv(Path(root, "results", state, "gsa", "parameters.csv"))
+            results = pd.read_csv(Path(root, "results", state, "ua", "results.csv"))
             check_metadata(metadata, params, results)
         except FileNotFoundError:
             continue
@@ -286,9 +300,9 @@ if __name__ == "__main__":
 
     dfs = []
     empty = True
-    for iso in ISOS:
-        iso_data = Path(root, "results", iso, "gsa", "SA")
-        filtered_data = Path(root, "dashboard", "data", "iso", iso, "sa.csv")
+    for state in STATES:
+        state_data = Path(root, "results", state, "gsa", "SA")
+        filtered_data = Path(root, "dashboard", "data", "state", state, "sa.csv")
 
         if not filtered_data.parent.exists():
             filtered_data.parent.mkdir(parents=True, exist_ok=True)
@@ -296,12 +310,12 @@ if __name__ == "__main__":
         if filtered_data.exists():
             sa = pd.read_csv(filtered_data, index_col=[0, 1])
             empty = sa.empty  # do not overwirte if already exists
-        elif not iso_data.exists():
-            logger.warning(f"No gsa model run data for '{iso}'")
+        elif not state_data.exists():
+            logger.warning(f"No gsa model run data for '{state}'")
             sa = get_empty_sa()
         else:
-            sa_names = get_result_names(root, iso, "gsa")
-            sa = collect_sa(root, iso, sa_names.keys())
+            sa_names = get_result_names(root, state, "gsa")
+            sa = collect_sa(root, state, sa_names.keys())
 
         if empty:
             sa.to_csv(filtered_data, index=True)
@@ -318,9 +332,9 @@ if __name__ == "__main__":
 
     dfs = []
     empty = True
-    for iso in ISOS:
-        iso_data = Path(root, "results", iso, "ua", "results")
-        filtered_data = Path(root, "dashboard", "data", "iso", iso, "ua_runs.csv")
+    for state in STATES:
+        state_data = Path(root, "results", state, "ua", "results")
+        filtered_data = Path(root, "dashboard", "data", "state", state, "ua_runs.csv")
 
         if not filtered_data.parent.exists():
             filtered_data.parent.mkdir(parents=True, exist_ok=True)
@@ -329,19 +343,19 @@ if __name__ == "__main__":
             ua = pd.read_csv(filtered_data, index_col=[0, 1])
             empty = ua.empty
 
-        elif not iso_data.exists():
-            logger.warning(f"No ua model run data for '{iso}'")
+        elif not state_data.exists():
+            logger.warning(f"No ua model run data for '{state}'")
             ua = get_empty_run()
 
         else:
-            result_names = get_result_names(root, iso, "ua")
-            ua = collect_runs(root, iso, "ua", result_names.keys())
+            result_names = get_result_names(root, state, "ua")
+            ua = collect_runs(root, state, "ua", result_names.keys())
 
         if empty:
             ua.to_csv(filtered_data, index=True)
 
         ua.to_csv(
-            Path(root, "dashboard", "data", "iso", iso, "ua_runs.csv"), index=True
+            Path(root, "dashboard", "data", "state", state, "ua_runs.csv"), index=True
         )
         dfs.append(ua)
 
@@ -356,16 +370,16 @@ if __name__ == "__main__":
 
     dfs = []
     base_params = pd.DataFrame()
-    for iso in ISOS:
-        iso_data = Path(root, "results", iso, "gsa")
-        if not iso_data.exists():
-            logger.warning(f"No parameter data for '{iso}'")
+    for state in STATES:
+        state_data = Path(root, "results", state, "gsa")
+        if not state_data.exists():
+            logger.warning(f"No parameter data for '{state}'")
         else:
             if base_params.empty:
-                base_params = get_base_params_file(root, iso)
+                base_params = get_base_params_file(root, state)
                 dfs.append(base_params)
-            iso_params = get_iso_params(root, iso)
-            dfs.append(iso_params)
+            state_params = get_state_params(root, state)
+            dfs.append(state_params)
     if not dfs:
         logger.error("No data found.")
         raise ValueError("No ISO data found.")
@@ -379,7 +393,7 @@ if __name__ == "__main__":
             "nice_name",
             "group",
             # "group_nice_name",
-            "iso",
+            "state",
             "component",
             "carrier",
             "attribute",
@@ -400,18 +414,18 @@ if __name__ == "__main__":
     # get nice names
 
     all_params = []
-    for iso in ISOS:
+    for state in STATES:
         try:
-            ua_params = get_param_names(root, iso, "ua")
+            ua_params = get_param_names(root, state, "ua")
             all_params.append(ua_params)
         except FileNotFoundError:
-            logger.debug(f"No ua nice names for {iso}.")
+            logger.debug(f"No ua nice names for {state}.")
             continue
 
         ua_params_expanded = get_ur_params_expanded(ua_params, params, metadata)
 
         with open(
-            Path(root, "dashboard", "data", "iso", iso, "ua_params.json"), "w"
+            Path(root, "dashboard", "data", "state", state, "ua_params.json"), "w"
         ) as f:
             json.dump(ua_params_expanded, f, indent=4)
 
@@ -425,17 +439,17 @@ if __name__ == "__main__":
         json.dump(combined_params, f, indent=4)
 
     # get the sample data
-    for iso in ISOS:
-        iso_data = Path(root, "results", iso, "ua", "sample_scaled.csv")
-        if not iso_data.exists():
-            logger.warning(f"No sample data for '{iso}'")
+    for state in STATES:
+        state_data = Path(root, "results", state, "ua", "sample_scaled.csv")
+        if not state_data.exists():
+            logger.warning(f"No sample data for '{state}'")
             continue
         else:
-            sample_data = pd.read_csv(iso_data)
+            sample_data = pd.read_csv(state_data)
             sample_data["run"] = sample_data.index
-            sample_data["iso"] = iso
-            sample_data = sample_data.set_index(["run", "iso"])
+            sample_data["state"] = state
+            sample_data = sample_data.set_index(["run", "state"])
             sample_data.to_csv(
-                Path(root, "dashboard", "data", "iso", iso, "sample_data.csv"),
+                Path(root, "dashboard", "data", "state", state, "sample_data.csv"),
                 index=True,
             )
