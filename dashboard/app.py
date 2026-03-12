6998c4f93fed6a994951697d643d3be7adcbadad
trevorb1 <trevor_barnes@sfu.ca>
2026-03-12 20:47:28 +0000

    caching dir

--- dashboard/app.py
+++ dashboard/app.py
@@ -91,21 +91,20 @@ import logging
 
 # Configure logging
 logging.basicConfig(
-    level=logging.DEBUG,
+    level=logging.WARNING,
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
     datefmt="%Y-%m-%d %H:%M:%S",
 )
 
 logger = logging.getLogger(__name__)
+cache_dir = os.environ.get("CACHE_DIR", "/mnt/dash_cache")
 
 app = DashProxy(
     __name__,
     external_stylesheets=[dbc.themes.BOOTSTRAP],
     suppress_callback_exceptions=True,
     transforms=[
-        ServersideOutputTransform(
-            backends=[FileSystemBackend(cache_dir="/tmp/dash_cache")]
-        )
+        ServersideOutputTransform(backends=[FileSystemBackend(cache_dir=cache_dir)])
     ],
 )
 
